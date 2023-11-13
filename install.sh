dd03c1af70590e9d6eac60091f2d87d99ab30037
Xiangfeng Zhu <xzhu0027@gmail.com>
2023-11-13 23:42:37 +0000

    minor update to install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+set -e
+
 # install python dependencies
 pip install lark pre-commit tomli tomli_w colorlog
 
@@ -5,3 +9,6 @@ COMPILER_DIR="$(dirname $(readlink -f "$0"))/compiler"
 ROOT_DIR=$(dirname $COMPILER_DIR)
 export PYTHONPATH=$PYTHONPATH:$COMPILER_DIR:$ROOT_DIR
 export PHOENIX_DIR="$HOME/phoenix"
+
+
+set +e
