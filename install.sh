fde7ba3b614463d3eed1a3fcdc688fd26d7b5d13
banruo <banruo@cs.washington.edu>
2023-11-23 04:07:34 +0000

    update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ set -e
 
 # install python dependencies
 sudo apt install -y python3-pip
-pip install lark pre-commit tomli tomli_w colorlog
+pip install lark pre-commit tomli tomli_w colorlog rich
 
 set +e
