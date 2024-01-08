8f92f85da7a15590bb43ffd86f927bed42a06b5b
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2024-01-07 23:14:06 -0500

    feat(envoy): turn off sidecar when idle

--- compiler/graph/backend/envoy.py
+++ compiler/graph/backend/envoy.py
@@ -19,46 +19,55 @@ def scriptgen_envoy(girs: Dict[str, GraphIR], app: str):
     os.makedirs(local_gen_dir, exist_ok=True)
 
     # compile elements
-    compiled_elements = set()
-    for gir in girs.values():
-        for element in gir.elements["req_client"] + gir.elements["req_server"]:
-            if element.lib_name not in compiled_elements:
-                compiled_elements.add(element.lib_name)
-                impl_dir = os.path.join(local_gen_dir, f"{element.lib_name}_envoy")
-                # compile
-                execute_local(
-                    [
-                        "cargo",
-                        "build",
-                        "--target=wasm32-wasi",
-                        "--manifest-path",
-                        os.path.join(impl_dir, "Cargo.toml"),
-                        "--release",
-                    ]
-                )
-                # copy binary to /tmp
-                execute_local(
-                    [
-                        "cp",
-                        os.path.join(
-                            impl_dir,
-                            f"target/wasm32-wasi/release/{element.lib_name}.wasm",
-                        ),
-                        "/tmp",
-                    ]
-                )
+    # compiled_elements = set()
+    # for gir in girs.values():
+    #     for element in gir.elements["req_client"] + gir.elements["req_server"]:
+    #         if element.lib_name not in compiled_elements:
+    #             compiled_elements.add(element.lib_name)
+    #             impl_dir = os.path.join(local_gen_dir, f"{element.lib_name}_envoy")
+    #             # compile
+    #             execute_local(
+    #                 [
+    #                     "cargo",
+    #                     "build",
+    #                     "--target=wasm32-wasi",
+    #                     "--manifest-path",
+    #                     os.path.join(impl_dir, "Cargo.toml"),
+    #                     "--release",
+    #                 ]
+    #             )
+    #             # copy binary to /tmp
+    #             execute_local(
+    #                 [
+    #                     "cp",
+    #                     os.path.join(
+    #                         impl_dir,
+    #                         f"target/wasm32-wasi/release/{element.lib_name}.wasm",
+    #                     ),
+    #                     "/tmp",
+    #                 ]
+    #             )
 
     # Copy filter binaries to worker nodes and generate mount scripts
     with open(
         os.path.join(BACKEND_CONFIG_DIR, "ping_pong_istio_template.yml"), "r"
     ) as f:
+        # complete manifest
         yml_list = list(yaml.safe_load_all(f))
+    with open(os.path.join(BACKEND_CONFIG_DIR, "ping_pong_template.yml"), "r") as f:
+        # manifest without istio
+        frontend_simple, ping_simple, pong_simple = list(yaml.safe_load_all(f))
+
     frontend_deploy, ping_deploy, pong_deploy = yml_list[1], yml_list[3], yml_list[5]
+    services = list(service_pos_dict[app].keys())
+    deploy_count = {sname: 0 for sname in services}
+
     for gir in girs.values():
         elist = [(e, gir.client) for e in gir.elements["req_client"]] + [
             (e, gir.server) for e in gir.elements["req_server"]
         ]
         for (element, sname) in elist:
+            deploy_count[sname] += 1
             copy_remote_host(
                 service_pos_dict[app][sname], f"/tmp/{element.lib_name}.wasm", "/tmp/"
             )
@@ -80,6 +89,12 @@ def scriptgen_envoy(girs: Dict[str, GraphIR], app: str):
                     "name": f"{element.lib_name}-wasm",
                 }
             )
+    # if a service has no elment attached, turn off its sidecar
+    for sname in services:
+        if deploy_count[sname] == 0:
+            target_yml = locals()[f"{sname}_deploy"]
+            target_yml.clear()
+            target_yml.update(locals()[f"{sname}_simple"])
     with open(os.path.join(local_gen_dir, "install.yml"), "w") as f:
         yaml.dump_all(yml_list, f, default_flow_style=False)
 
