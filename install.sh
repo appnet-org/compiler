e4719a814af7c56c81c990cef31f0b96c0eec275
Yuyao <yuyaowang_kristoff@outlook.com>
2024-01-07 21:38:57 -0500

    fix: pyyaml package name

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ set -e
 
 # install python dependencies
 sudo apt install -y python3-pip
-pip install lark pre-commit tomli tomli_w colorlog rich kubernetes yaml
+pip install lark pre-commit tomli tomli_w colorlog rich kubernetes pyyaml
 
 set +e
